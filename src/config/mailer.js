7c5c28b7f178e5e836fd74e8ca6ca1f45fbc00da
Aman-Kumar-Sharma <amanksharma987@gmail.com>
2025-07-23 13:42:48 +0530

    production ready

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -26,3 +26,29 @@ transporter.verify((error, success) => {
 });
 
 module.exports = transporter;
+
+
+// const nodemailer = require('nodemailer');
+
+// const transporter = nodemailer.createTransport({
+//     host: process.env.SMTP_HOST,
+//     port: parseInt(process.env.SMTP_PORT, 10),
+//     secure: parseInt(process.env.SMTP_PORT, 10) === 465, // true for 465, false for other ports like 587
+//     auth: {
+//         user: process.env.SMTP_USER,
+//         pass: process.env.SMTP_PASS,
+//     },
+//     tls: {
+//         ciphers:'SSLv3'
+//     }
+// });
+
+// transporter.verify((error, success) => {
+//     if (error) {
+//         console.error('Mail server configuration error:', error);
+//     } else {
+//         console.log('Mail server is ready to take our messages');
+//     }
+// });
+
+// module.exports = transporter;
